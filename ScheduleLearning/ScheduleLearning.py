f222c47adc21feaa9e3d559e7a26b321b83d8e17
Mikhail <mikhailethanram@gmail.com>
2020-08-29 16:40:38 +0530

    Cleaned up script and changed the algorithm to make it consider time on a weekly basis. Also changed the data input and output file paths.

--- ScheduleLearning/ScheduleLearning.py
+++ ScheduleLearning/ScheduleLearning.py
@@ -1,115 +1,181 @@
+# Importing all the required modules
 import json
-from datetime import datetime, timezone
 import numpy as np
+import pandas as pd
+from datetime import datetime, timezone
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
-import pandas as pd
 import geopy
 from geopy.geocoders import Nominatim
+import scipy.cluster.hierarchy as sch
+from sklearn.cluster import AgglomerativeClustering
+import gmplot
+from sklearn.neighbors.nearest_centroid import NearestCentroid
 
-locator = Nominatim(user_agent='google')
-def get_address(lat, long):
-    coordinates = str(lat) + ", " + str(long)
+def loadData(path):
+    with open(path) as f:
+        data = json.load(f)
+    return data
+    print("JSON Load Successful!")
+
+def convertToTimestamp(datetimeObject):
+    return datetimeObject.replace(tzinfo=timezone.utc).timestamp()
+
+def convertToUTC(timestamp):
+    return datetime.utcfromtimestamp(timestamp)
+
+def getMeanFromList(values):
+    mean = sum(values)/len(values)
+    return mean
+
+def roundTimestamp(dt_series, seconds=60, up=False):
+    return dt_series // seconds * seconds + seconds * up
+
+def getAddress(latitude, longitude):
+    coordinates = str(latitude) + ", " + str(longitude)
     location = locator.reverse(coordinates)
     return location.address
 
-#place your history file in your project deirectory
-with open("Takeout/Location History/Location History.json") as f:
-    data = json.load(f)
-    print("JSON Load Successful!")
+def getCentroids(data, predictions):
+    clf = NearestCentroid()
+    clf.fit(data, predictions)
+    return clf.centroids_
+#states = ['STILL', 'IN_VEHICLE', 'ON_FOOT', 'ON_BICYCLE', 'TILTING', 'UNKNOWN', 'EXITING_VEHICLE']
+def createDataStructure(data):
+    latitudes = []
+    longitudes = []
+    timestamps = []
+    allowedStates = ['STILL']
+    transitionalState = False
+    for i in range(len(data['locations'])):
+        entry = data["locations"][i]
+        try:
+            if(entry['activity'][0]['activity'][0]['type'] not in allowedStates):
+                transitionalState = True
+            else:
+                transitionalState = False
+        except:
+            pass
+        timeToAppend = roundTimestamp(int(entry["timestampMs"][:10]), 60*60)
+        latitudeToAppend = entry["latitudeE7"]/(10**7)
+        longitudeToAppend = entry["longitudeE7"]/(10**7)
+        while(timeToAppend > day8):
+            timeToAppend -= oneWeek
+        if(len(latitudes) == 0 and not transitionalState):
+            timestamps.append(timeToAppend)
+            latitudes.append(latitudeToAppend)
+            longitudes.append(longitudeToAppend)
+        elif(timeToAppend > timestamps[-1] and not transitionalState):
+            timestamps.append(timeToAppend)
+            latitudes.append(latitudeToAppend)
+            longitudes.append(longitudeToAppend)
+            #time = convertToUTC(ts)
+    dataStructure = np.array(list(zip(timestamps, latitudes, longitudes)))
+    return dataStructure, latitudes, longitudes, timestamps
 
+def createAndShowDendogram(data):
+    dendrogram = sch.dendrogram(sch.linkage(data, method = 'ward'))
+    plt.title('Dendrogram')
+    plt.xlabel('Points')
+    plt.ylabel('Euclidean distances')
+    plt.show()
 
-dtstart = datetime(2020, 7, 1, 1, 0, 0)
-dtend = datetime(2020, 8, 1, 1, 0, 0)
-timestampstart = dtstart.replace(tzinfo=timezone.utc).timestamp()
-timestampend = dtend.replace(tzinfo=timezone.utc).timestamp()
-#print(timestamp)
+def createModelAndPredict(data, clusters):
+    model = AgglomerativeClustering(n_clusters = clusters, affinity = 'euclidean', linkage = 'ward')
+    modelPredictions = model.fit_predict(X)
+    return model, modelPredictions
 
-lats = []
-longs = []
-tmstps = []
+def plotandShow3dGraph(data, predictions):
+    fignum = 1
+    fig = plt.figure(fignum, figsize=(10, 9))
+    colors = ['red', 'blue', 'yellow', 'violet']
+    centroid_color = "green"
+    ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=30, azim=50)
+    for i in range(len(centroids)):
+        ax.scatter(data[predictions == i, 0], data[predictions == i, 1], data[predictions == i, 2], s=5, c=colors[i], label = 'Cluster ' + str(i+1))
+        ax.scatter(centroids[i][0], centroids[i][1], centroids[i][2], s=500, c=centroid_color, label = 'Centroids')
+    ax.w_xaxis.set_ticklabels([])
+    ax.w_yaxis.set_ticklabels([])
+    ax.w_zaxis.set_ticklabels([])
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Latitude')
+    ax.set_zlabel('Longitude')
+    ax.set_title('Location over time')
+    ax.dist = 10
+    ax.legend()
+    plt.show()
 
-def round_unix_date(dt_series, seconds=60, up=False):
-    return dt_series // seconds * seconds + seconds * up
+def createMap(filePath, latitudes, longitudes):
+    zoom = 10
+    meanLatitude = getMeanFromList(latitudes)
+    meanLongitude = getMeanFromList(longitudes)
+    gmap3 = gmplot.GoogleMapPlotter(meanLatitude, meanLongitude, zoom)
+    gmap3.scatter(latitudes, longitudes, 'cornflowerblue', size = 1000, marker = False)
+    for i in range(len(centroids)):
+        gmap3.scatter([float(centroids[i][1])], [float(centroids[i][2])], 'green', size = 5000, marker = False)
+    gmap3.draw(filePath)
 
-#retrieving the lats and longs
-for entry in data["locations"]:
-    if(len(lats) == 0 and int(entry["timestampMs"][:10]) > timestampstart and int(entry["timestampMs"][:10]) < timestampend):
-        tmstps.append(round_unix_date(int(entry["timestampMs"][:10]), 60*60))
-        lats.append(entry["latitudeE7"]/(10**7))
-        longs.append(entry["longitudeE7"]/(10**7))
-    else:
-        if(int(entry["timestampMs"][:10]) > timestampstart and int(entry["timestampMs"][:10]) < timestampend and round_unix_date(int(entry["timestampMs"][:10]), 60*60) > tmstps[-1]):
-            tmstps.append(round_unix_date(int(entry["timestampMs"][:10]), 60*60))
-            lats.append(entry["latitudeE7"]/(10**7))
-            longs.append(entry["longitudeE7"]/(10**7))
-            #time = datetime.utcfromtimestamp(ts)
+locator = Nominatim(user_agent='google')
+filePath = "TakeoutSR/Location History/Location History.json"
+data = loadData(filePath)
+len(data['locations'])
+state = ['']
+for i in range(len(data['locations'])):
+    try:
+        if(data['locations'][i]['activity'][0]['activity'][0]['type'] not in state):
+            state.append(data['locations'][i]['activity'][0]['activity'][0]['type'])
+    except:
+        pass
 
-X = np.array(list(zip(tmstps, lats, longs)))
+startDate = datetime(2017, 1, 1, 0, 0, 0)
+endDate = datetime(2017, 6, 1, 0, 0, 0)
+timestampstart = convertToTimestamp(startDate)
+timestampend = convertToTimestamp(endDate)
+day1 = roundTimestamp(int(timestampstart), 24*60*60)
+oneWeek = 7*24*60*60
+day8 = day1 + oneWeek
+X_init, latitudes_init, longitudes_init, timestamps = createDataStructure(data)
+X_init[0]
+X = []
+latitudes = []
+longitudes = []
+for i in range(len(X_init)):
+    if(X_init[i][0] > timestampstart and X_init[i][0] < timestampend):
+        X.append(X_init[i])
+        latitudes.append(X_init[i][1])
+        longitudes.append(X_init[i][2])
+X = np.array(X)
 
-#print(X)
-#print(data['locations'][0]['latitudeE7']/(10**7))
+convertToUTC(X_init[0][0])
+convertToUTC(X_init[-1][0])
+len(X)
+#convertToUTC(X[-1][0])
+#convertToUTC(int(data['locations'][0]['timestampMs'][:10]))
+createAndShowDendogram(X)
+model, predictions = createModelAndPredict(X, 2)
+centroids = getCentroids(X, predictions)
+plotandShow3dGraph(X, predictions)
+createMap("hc_map_2020_7_2020_8_SR.html", latitudes, longitudes)
 
-'''import scipy.cluster.hierarchy as sch
-dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
-plt.title('Dendrogram')
-plt.xlabel('Points')
-plt.ylabel('Euclidean distances')
-plt.show()'''
+for i in range(len(centroids)):
+    print(getAddress(centroids[i][1], centroids[i][2]))
 
-from sklearn.cluster import AgglomerativeClustering
-clusters = 2
-hc = AgglomerativeClustering(n_clusters = clusters, affinity = 'euclidean', linkage = 'ward')
-y_hc = hc.fit_predict(X)
-
-from sklearn.neighbors.nearest_centroid import NearestCentroid
-clf = NearestCentroid()
-clf.fit(X, y_hc)
-centroids = clf.centroids_
-
-# Plot the 3D representation
-fignum = 1
-fig = plt.figure(fignum, figsize=(10, 9))
-ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=30, azim=50)
-
-ax.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], X[y_hc == 0, 2], s=5, c='red', label = 'Cluster 1')
-ax.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], X[y_hc == 1, 2], s=5, c='blue', label = 'Cluster 2')
-#ax.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], X[y_hc == 2, 2], s=5, c='yellow', label = 'Cluster 3')
-#ax.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], X[y_hc == 3, 2], s=5, c='violet', label = 'Cluster 4')
-for i in range(clusters):
-    ax.scatter(centroids[i][0], centroids[i][1], centroids[i][2], s=500, c='green', label = 'Centroids')
-ax.w_xaxis.set_ticklabels([])
-ax.w_yaxis.set_ticklabels([])
-ax.w_zaxis.set_ticklabels([])
-ax.set_xlabel('Time')
-ax.set_ylabel('Latitude')
-ax.set_zlabel('Longitude')
-ax.set_title('Location over time')
-ax.dist = 10
-ax.legend()
-
-plt.show()
-for i in range(clusters):
-    print(get_address(centroids[i][1], centroids[i][2]))
-#print(get_address(hc.cluster_centers_[3, 1], hc.cluster_centers_[3, 2]))
-
-#hc.cluster_centers_[2, 1]
-#hc.cluster_centers_[2, 2]
-#import plotly.express as px
-#px.set_mapbox_access_token(open(".mapbox_token").read())
-#coords = pd.DataFrame(hc.cluster_centers_, columns=['time', 'lat', 'long'])
-#map = px.scatter_mapbox(coords, lat='lat', lon='long', size_max=15, zoom=10)
-#map.update_layout(mapbox_style="open-street-map")
-#map.show()
-
-# import gmplot package
-import gmplot
-
-gmap3 = gmplot.GoogleMapPlotter(sum(lats)/len(lats), sum(longs)/len(longs), 7)
-
-# scatter method of map object
-# scatter points on the google map
-gmap3.scatter(lats, longs, 'cornflowerblue', size = 2000, marker = False)
-for i in range(clusters):
-    gmap3.scatter([float(centroids[i][1])], [float(centroids[i][2])], 'green', size = 20000, marker = False)
-gmap3.draw("hc_map_2020_07_2020_08_MR.html")
+'''
+latitudes1 = []
+timestamps1 = []
+for i in range(1000):
+    time = roundTimestamp(int(data["locations"][i]["timestampMs"][:10]), 60*60)
+    #print("Initial Time: "+str(time))
+    while(time > day1 + 7*24*60*60):
+        time -= 7*24*60*60
+    if(len(latitudes1) == 0 and time < timestampend):
+        print("Appended Time First: "+str(time))
+        print(convertToUTC(time))
+        timestamps1.append(time)
+        latitudes1.append(data["locations"][i]["latitudeE7"]/(10**7))
+    elif(time < timestampend and time > timestamps1[-1]):
+        print("Appended Time: "+str(time))
+        print(convertToUTC(time))
+        timestamps1.append(time)
+        latitudes1.append(data["locations"][i]["latitudeE7"]/(10**7))
+'''
