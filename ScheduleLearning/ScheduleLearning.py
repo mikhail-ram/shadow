396764083412117524c6413127961c9c39dca3c7
Mikhail <mikhailethanram@gmail.com>
2020-09-01 19:34:06 +0530

    Improved 3d graphs by switching to plotly express. Major data overhaul using pandas to clean data instead of numpy. Changed convertToUTC function to return dates of timestamp instead of a dateTime object. Added states column to pandas dataFrame.

--- ScheduleLearning/ScheduleLearning.py
+++ ScheduleLearning/ScheduleLearning.py
@@ -24,7 +24,8 @@ def convertToTimestamp(datetimeObject):
     return datetimeObject.replace(tzinfo=timezone.utc).timestamp()
 
 def convertToUTC(timestamp):
-    return datetime.utcfromtimestamp(timestamp)
+    datetimeObject = datetime.utcfromtimestamp(timestamp)
+    return datetimeObject.strftime("%Y-%m-%d")
 
 def getMeanFromList(values):
     mean = sum(values)/len(values)
@@ -39,45 +40,44 @@ def getAddress(latitude, longitude):
     return location.address
 
 def getCentroids(data, predictions):
-    clf = NearestCentroid()
-    clf.fit(data, predictions)
-    return clf.centroids_
+    centroidLocator = NearestCentroid()
+    centroidLocator.fit(data, predictions)
+    centroids = pd.DataFrame(centroidLocator.centroids_, columns = ["Timestamps", "Latitudes", "Longitudes"])
+    for i in range(len(centroids)):
+        centroids.iloc[i, 0] = convertToUTC(centroids.iloc[i, 0])
+    return centroids
 
 #states = ['STILL', 'IN_VEHICLE', 'ON_FOOT', 'ON_BICYCLE', 'TILTING', 'UNKNOWN', 'EXITING_VEHICLE']
 def createDataStructure(data):
+    timestamps = []
     latitudes = []
     longitudes = []
-    timestamps = []
-    allowedStates = ['STILL']
-    transitionalState = False
+    states = []
+    allowedStates = ['STILL', 'IN_VEHICLE', 'ON_FOOT', 'ON_BICYCLE', 'TILTING', 'UNKNOWN', 'EXITING_VEHICLE', 'UNDEFINED']
     for i in range(len(data['locations'])):
         entry = data["locations"][i]
-        try:
-            if(entry['activity'][0]['activity'][0]['type'] not in allowedStates):
-                transitionalState = True
-            else:
-                transitionalState = False
-        except:
-            pass
         timeToAppend = roundTimestamp(int(entry["timestampMs"][:10]), 60*60)
         latitudeToAppend = entry["latitudeE7"]/(10**7)
         longitudeToAppend = entry["longitudeE7"]/(10**7)
-        while(timeToAppend > day8):
-            timeToAppend -= oneWeek
-        if(len(latitudes) == 0 and len(timestamps) == 0 and timeToAppend is not None and not transitionalState):
-            timestamps.append(timeToAppend)
-            latitudes.append(latitudeToAppend)
-            longitudes.append(longitudeToAppend)
-        else:
-            try:
-                if(timeToAppend > timestamps[-1] and not transitionalState):
-                    timestamps.append(timeToAppend)
-                    latitudes.append(latitudeToAppend)
-                    longitudes.append(longitudeToAppend)
-            except IndexError:
-                print("No Values that match criteria!")
-                pass
-    dataStructure = np.array(list(zip(timestamps, latitudes, longitudes)))
+        timestamps.append(timeToAppend)
+        latitudes.append(latitudeToAppend)
+        longitudes.append(longitudeToAppend)
+        try:
+            states.append(entry['activity'][0]['activity'][0]['type'])
+        except KeyError:
+            states.append("UNDEFINED")
+    dataStructure = pd.DataFrame(list(zip(timestamps, latitudes, longitudes, states)), columns=['Timestamps', 'Latitudes', 'Longitudes', 'States'])
+    dataStructure.sort_values("Timestamps", inplace = True)
+    dataStructure = dataStructure[dataStructure['Timestamps'].between(timestampstart, timestampend)]
+    dataStructure = dataStructure[dataStructure['States'].isin(allowedStates)]
+    dataStructure.drop_duplicates(subset = ["Timestamps"], keep = 'first', inplace = True)
+    dataStructure.reset_index(drop=True, inplace=True)
+    for i in range(len(dataStructure)):
+        timeToCorrect = dataStructure.iloc[i, 0]
+        while(timeToCorrect > day8):
+            timeToCorrect -= oneWeek
+        dataStructure.iloc[i, 0] = timeToCorrect
+    dataStructure.reset_index(drop=True, inplace=True)
     return dataStructure, latitudes, longitudes, timestamps
 
 def createAndShowDendogram(data):
@@ -88,68 +88,19 @@ def createAndShowDendogram(data):
     plt.show()
 
 def createModelAndPredict(data, clusters):
-    model = AgglomerativeClustering(n_clusters = clusters, affinity = 'euclidean', linkage = 'ward')
-    modelPredictions = model.fit_predict(X)
+    model = AgglomerativeClustering(n_clusters=clusters, affinity='euclidean', linkage='ward')
+    modelPredictions = model.fit_predict(data)
     return model, modelPredictions
 
-
-
-#import plotly.plotly as py
-import plotly.graph_objs as go
-from plotly import offline
-offline.init_notebook_mode()
-
+import plotly.express as px
 def plotandShow3dGraph(data, predictions):
-
-    scatter_data = [go.Scatter3d(x=timestamps, y=latitudes, z=longitudes)]
-
-    layout = go.Layout(
-        scene=go.layout.Scene(
-            xaxis=go.layout.scene.XAxis(
-                spikecolor='#1fe5bd',
-                spikesides=False,
-                spikethickness=6,
-            ),
-            yaxis=go.layout.scene.YAxis(
-                spikecolor='#1fe5bd',
-                spikesides=False,
-                spikethickness=6,
-            ),
-            zaxis=go.layout.scene.ZAxis(
-                spikecolor='#1fe5bd',
-                spikethickness=6,
-            ),
-        ),
-    )
-    fig = go.Figure(data=scatter_data, layout=layout)
-    fig.update_layout(scene = dict(
-                        xaxis_title='Timestamps',
-                        #xaxis = dict(range=[timestampstart,timestampend]),
-                        yaxis_title='Latitudes',
-                        zaxis_title='Longitudes'),
-                        width=700,
-                        margin=dict(r=20, b=10, l=10, t=10))
-    offline.iplot(fig)
-
-    fignum = 1
-    fig = plt.figure(fignum, figsize=(10, 9))
-    colors = ['red', 'blue', 'yellow', 'violet']
-    centroid_color = "green"
-    ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=30, azim=50)
-    for i in range(len(centroids)):
-        ax.scatter(data[predictions == i, 0], data[predictions == i, 1], data[predictions == i, 2], s=5, c=colors[i], label = 'Cluster ' + str(i+1), picker=True)
-        ax.scatter(centroids[i][0], centroids[i][1], centroids[i][2], s=500, c=centroid_color, label = 'Centroids')
-    ax.w_xaxis.set_ticklabels([])
-    ax.w_yaxis.set_ticklabels([])
-    ax.w_zaxis.set_ticklabels([])
-    ax.set_xlabel('Time')
-    ax.set_ylabel('Latitude')
-    ax.set_zlabel('Longitude')
-    ax.set_title('Location over time')
-    ax.dist = 10
-    ax.legend()
-
-    plt.show()
+    fig = px.scatter_3d(data, x=dates, y='Latitudes', z='Longitudes', color=predictions)
+    fig.update_traces(marker=dict(size=5, line=dict(width=1, color='DarkSlateGrey')), selector=dict(mode='markers'))
+    centroidPlot = px.scatter_3d(centroids, x='Timestamps', y='Latitudes', z='Longitudes')
+    centroidPlot.update_traces(marker=dict(size=20, line=dict(width=1, color='DarkSlateGrey')), selector=dict(mode='markers'))
+    fig.add_trace(centroidPlot.data[0])
+    fig.update_layout(font_family="Akrobat", title_font_family="Akrobat", hoverlabel=dict(font_family="Akrobat"), scene=dict(xaxis_title='Dates'))
+    fig.show()
 
 def createMap(filePath, latitudes, longitudes):
     zoom = 10
@@ -162,46 +113,25 @@ def createMap(filePath, latitudes, longitudes):
     gmap3.draw(filePath)
 
 locator = Nominatim(user_agent='google')
-filePath = "TakeoutAR/Location History/Location History.json"
+filePath = "TakeoutSR/Location History/Location History.json"
 data = loadData(filePath)
-len(data['locations'])
-state = ['']
-for i in range(len(data['locations'])):
-    try:
-        if(data['locations'][i]['activity'][0]['activity'][0]['type'] not in state):
-            state.append(data['locations'][i]['activity'][0]['activity'][0]['type'])
-    except:
-        pass
-
-startDate = datetime(2018, 6, 1, 0, 0, 0)
-endDate = datetime(2018, 7, 1, 0, 0, 0)
+startDate = datetime(2014, 6, 1, 0, 0, 0)
+endDate = datetime(2014, 7, 1, 0, 0, 0)
 timestampstart = convertToTimestamp(startDate)
 timestampend = convertToTimestamp(endDate)
 day1 = roundTimestamp(int(timestampstart), 24*60*60)
-oneWeek = 7*24*60*60
+oneWeek = 6*24*60*60
 day8 = day1 + oneWeek
-X_init, latitudes_init, longitudes_init, timestamps = createDataStructure(data)
-X_init[0]
-X = []
-latitudes = []
-longitudes = []
-for i in range(len(X_init)):
-    if(X_init[i][0] > timestampstart and X_init[i][0] < timestampend):
-        X.append(X_init[i])
-        latitudes.append(X_init[i][1])
-        longitudes.append(X_init[i][2])
-X = np.array(X)
+X, latitudes, longitudes, timestamps = createDataStructure(data)
+X_np = X.iloc[:, :-1].values
+#createAndShowDendogram(X_np)
+model, predictions = createModelAndPredict(X_np, 2)
+centroids = getCentroids(X_np, predictions)
+dates = X.iloc[:, 0]
+for i in range(len(dates)):
+    dates.iloc[i] = convertToUTC(dates.iloc[i])
 
-convertToUTC(X_init[0][0])
-convertToUTC(X_init[-1][0])
-len(X)
-#convertToUTC(X[-1][0])
-#convertToUTC(int(data['locations'][0]['timestampMs'][:10]))
-createAndShowDendogram(X)
-model, predictions = createModelAndPredict(X, 2)
-centroids = getCentroids(X, predictions)
 plotandShow3dGraph(X, predictions)
-createMap("hc_map_2020_07_2020_08_AR_test.html", latitudes, longitudes)
-X[-1]
+createMap("hc_map_SR_test.html", latitudes, longitudes)
 for i in range(len(centroids)):
     print(getAddress(centroids[i][1], centroids[i][2]))
